6e5cb5213382b60e1fc5cb394c5af031395bc721
rch850 <rich850@gmail.com>
2022-07-30 07:28:33 +0000

    yaruzo -m

--- yaruzo.sh
+++ yaruzo.sh
@@ -12,6 +12,6 @@ read -p 'Please run benchmark. Press enter key when finished.'
 
 sudo cat /var/log/nginx/access.log > ~/logs/${LOG_PREFIX}-access.log
 sudo cat /var/log/mysql/mysql-slow.log > /tmp/${LOG_PREFIX}-mysql-slow.log
-alp ltsv --file ~/logs/${LOG_PREFIX}-access.log --reverse --sort=avg > ~/logs/${LOG_PREFIX}-alp.log
+alp ltsv --file ~/logs/${LOG_PREFIX}-access.log --reverse --sort=avg -m '/api/player/player/,/api/player/competition/,/api/organizer/player/,/api/organizer/competition/' > ~/logs/${LOG_PREFIX}-alp.log
 pt-query-digest /tmp/${LOG_PREFIX}-mysql-slow.log > ~/logs/${LOG_PREFIX}-ptq.log
 
